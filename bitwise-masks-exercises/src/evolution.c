8a7fe15786b867a585f3f686d8140fc72534fe3a
Denis <denis.barzanov2002@gmail.com>
2018-03-05 16:29:35 +0200

    Changes to evolution.c

--- bitwise-masks-exercises/src/evolution.c
+++ bitwise-masks-exercises/src/evolution.c
@@ -4,11 +4,11 @@
 
 uint64_t evolution(uint64_t state) {
 	uint64_t new_state = 0;
-	for (int i = 0; i < sizeof(uint32_t) * 8 - 2; i++) { /* -2 for excluding 
+	for (int i = 0; i < sizeof(uint64_t) * 8 - 2; i++) { /* -2 for excluding 
 							the last 2 iterations */
-		uint64_t current_indexed_bits = 0b111 << i;
-		uint64_t current_state_block = state & current_indexed_bits;
-		uint8_t evolved_bit = get_evolved_bit(current_state_block >> i);
+		//uint64_t current_indexed_bits = 0b111ul << i;
+		//uint64_t current_state_block = state & current_indexed_bits;
+		uint8_t evolved_bit = get_evolved_bit((state >> i) & 0b111);
 		set_evolved_bit_to_new_state(&new_state, evolved_bit, i + 1);
 	}
 	return new_state;
@@ -32,5 +32,10 @@ uint8_t get_evolved_bit(uint8_t bits_3) {
 
 
 void set_evolved_bit_to_new_state(uint64_t* new_state, uint8_t bit, uint8_t index) {
-	*new_state = (*new_state & (~(1 << index))) | (bit << index);
+	// *new_state = (*new_state & (~(1 << index))) | (bit << index);
+	if (bit == 0) {
+		*new_state &= ~((uint64_t)1 << index);
+	} else {
+		*new_state |= (uint64_t)1 << index;
+	}
 }
